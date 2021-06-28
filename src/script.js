c9befefa6df68e48f7ad1d26523782ad94812990
4zorbin <adwell83@gmail.com>
2021-06-28 23:33:21 +0300

    fix the result after count sum

--- src/script.js
+++ src/script.js
@@ -6,11 +6,14 @@ let count = 0
 function increment(){
     count = count + 1
     console.log(count)
-    countEl.innerText = count
+    countEl.textContent = count
     // document.getElementById("count-el").innerText = count
 }
     // fun save to log out the count when it's called
 function save() {
    console.log(count)
-   saveEl.innerText += " " + count + " + "
+   //saveEl.innerText += " " + count + " + "
+   saveEl.textContent += count + " + "
+  count = 0
+  countEl.textContent = 0
 }
