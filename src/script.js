d3bc857ac3ed493d2ddeb31c4ac0956544228d96
4zorbin <adwell83@gmail.com>
2021-06-29 00:41:27 +0300

    set clear btn and style css with color transparent

--- src/script.js
+++ src/script.js
@@ -1,19 +1,26 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+
+
     // console.log(countEl)
 let count = 0
 
 function increment(){
     count = count + 1
-    console.log(count)
+    console.log("inc" + count)
     countEl.textContent = count
-    // document.getElementById("count-el").innerText = count
+    // document.getElementById("count-el").innerText = count ------ as option
 }
     // fun save to log out the count when it's called
 function save() {
-   console.log(count)
-   //saveEl.innerText += " " + count + " + "
+   console.log("save" + count)
+   //saveEl.innerText += " " + count + " + "  ----as option
    saveEl.textContent += count + " + "
   count = 0
   countEl.textContent = 0
+}
+    // clear the result in bottom field
+function clearRes() {
+    saveEl.textContent = "Previous: "
+   console.log("clear" + saveEl)
 }
