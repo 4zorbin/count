c708709f01e3a74a87ba6a031a1f63aa049830f4
4zorbin <adwell83@gmail.com>
2021-06-28 13:44:23 +0300

    add js count fun

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5
+
+
+let count = 0
+
+function increment(){
+    count = count + 1
+    console.log(count)
+    document.getElementById("count-el").innerText = count
+}
