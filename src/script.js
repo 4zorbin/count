ffbf07d38b8c4f525a906689da2c0f14e8c2d507
4zorbin <adwell83@gmail.com>
2021-06-29 03:20:50 +0300

    fix del some spaces'

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
-
-    // console.log(countEl)
 let count = 0
 
-function increment(){
+function increment() {
     count = count + 1
     console.log("inc" + count)
     countEl.textContent = count
